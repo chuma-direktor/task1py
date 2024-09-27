29444c937a81f023be0fb2149cc0dd777fae79bc
chuma-direktor <altaichuma04@gmail.com>
2024-09-27 15:24:56 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+print('12 + 89 - 1')
+
+# "1st program"
+print(9 ** 0.5 * 5 )
+
+# "2st program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# "3st program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2+2))
+
+# "4st program"
+'123.456'
+print(123.456 * 10)
+print(int(123.456 * 10) / 10)
